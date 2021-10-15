fb143112c4e7130b6b862cb575aea1bb7e5026a2
Juan Mora <juanvalentin.mora@globant.com>
2021-10-15 18:24:16 -0300

    se agrega modelo de python y de jupiter

---
+++ main.py
@@ -0,0 +1,102 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+(X_entrenamiento, Y_entrenamiento), (X_pruebas, Y_pruebas) = mnist.load_data()
+
+class_names = ['Cero', 'Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco', 'Seis', 'Siete', 'Ocho', 'Nueve']
+
+#Colocar los datos en la forma correcta que ya hemos visto (1, 28, 28, 1)
+X_entrenamiento = X_entrenamiento.reshape(X_entrenamiento.shape[0], 28, 28, 1)
+X_pruebas = X_pruebas.reshape(X_pruebas.shape[0], 28, 28, 1)
+
+#Hacer 'one-hot encoding' de los resultados (e.g. en lugar de tener como resultado una sola neurona, tendre 10 donde solo el resultado correcto sera 1 y el resto 0)
+Y_entrenamiento = to_categorical(Y_entrenamiento)
+Y_pruebas = to_categorical(Y_pruebas)
+
+#Convertir a flotante y normalizar para que aprenda mejor la red
+X_entrenamiento = X_entrenamiento.astype('float32') / 255
+X_pruebas = X_pruebas.astype('float32') / 255
+
+#Aumento de datos
+#Variables para controlar las transformaciones que se haran en el aumento de datos
+#utilizando ImageDataGenerator de keras
+
+rango_rotacion = 30
+mov_ancho = 0.25
+mov_alto = 0.25
+#rango_inclinacion=15 #No uso este de momento pero si quieres puedes probar usandolo!
+rango_acercamiento=[0.5,1.5]
+
+datagen = ImageDataGenerator(
+    rotation_range = rango_rotacion,
+    width_shift_range = mov_ancho,
+    height_shift_range = mov_alto,
+    zoom_range=rango_acercamiento,
+    #shear_range=rango_inclinacion #No uso este de momento pero si quieres puedes probar usandolo!
+)
+
+datagen.fit(X_entrenamiento)
+
+#Modelo!
+modelo = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(100, activation='relu'),
+    tf.keras.layers.Dense(10, activation="softmax")
+])
+
+#Compilación
+modelo.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+#Los datos para entrenar saldran del datagen, de manera que sean generados con las transformaciones que indicamos
+data_gen_entrenamiento = datagen.flow(X_entrenamiento, Y_entrenamiento, batch_size=32)
+
+TAMANO_LOTE = 32
+
+#Entrenar la red.
+print("Entrenando modelo...");
+epocas = 60
+history = modelo.fit(
+    data_gen_entrenamiento,
+    epochs=epocas,
+    batch_size=TAMANO_LOTE,
+    validation_data=(X_pruebas, Y_pruebas),
+    steps_per_epoch=int(np.ceil(60000 / float(TAMANO_LOTE))),
+    validation_steps=int(np.ceil(10000 / float(TAMANO_LOTE)))
+)
+print("Modelo entrenado!");
+
+imagen = X_pruebas[2]
+number = imagen
+number = np.array([number])
+prediccion = modelo.predict(number)
+print("Predicción: " + class_names[np.argmax(prediccion[0])])
+
+#Visor de la imagen a evaluar
+plt.figure(figsize=(4, 4))
+plt.grid(False)
+plt.xticks([])
+plt.yticks([])
+plt.imshow(imagen, cmap=plt.cm.binary)
+print("Predicción: " + class_names[np.argmax(prediccion[0])])
+plt.show()
+
+for i in range(9):
+    number = X_pruebas[i]
+    number = np.array([number])
+    plt.subplot(330 + 1 + i)
+    plt.imshow(X_pruebas[i], cmap=plt.get_cmap('gray'))
+    prediccion = modelo.predict(number)
+    plt.ylabel(class_names[np.argmax(prediccion[0])])
+plt.show()
